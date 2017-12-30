4a8c114dfb28c85ae8a72fada44709e9966d5e7f
y10 <serhiy.voytenko@gmail.com>
2017-12-29 19:46:57 -0500

    Enanced WS module

--- html/js/services/wss-service.js
+++ html/js/services/wss-service.js
@@ -1,43 +1,48 @@
 Wss = (function () {
 
     var onSuccess = [];
+
     var onError = [];
 
     var websock = null;
 
+    var enabled = false;
+
+    try {
+        websock = new WebSocket('ws://' + window.location.hostname + ':80/ws');
+        websock.onopen = function (evt) { console.log('WS: open'); };
+        websock.onclose = function (evt) { console.log('WS: close'); };
+        websock.onerror = function (evt) {
+            console.log("WS: error");
+            console.log(evt);
+            if(enabled)
+            {
+                onError.forEach(function (callback) {
+                    callback(evt);
+                }, this);
+            }
+        };
+        websock.onmessage = function (evt) {
+            console.log(evt);
+            if(enabled)
+            {
+                var state = JSON.parse(evt.data);
+                onSuccess.forEach(function (callback) {
+                    callback(state);
+                }, this);
+            }
+        };
+
+        enabled = true;
+    }
+    catch (error) 
+    {
+        console.log(error);
+    }
+
     return {
 
         on: function (onSuccessCallback, onErrorCallback) {
-
-            if (!websock)
-            {
-                try {
-                    websock = new WebSocket('ws://' + window.location.hostname + ':80/ws');
-                    websock.onopen = function (evt) { console.log('WS: open'); };
-                    websock.onclose = function (evt) { console.log('WS: close'); };
-                
-                    websock.onerror = function (evt) {
-                        console.log("WS: error");
-                        console.log(evt);
-                        
-                        onError.forEach(function (callback) {
-                            callback(evt);
-                        }, this);
-                    };
-                
-                    websock.onmessage = function (evt) {
-                        console.log(evt);
-                
-                        var state = JSON.parse(evt.data);
-                        onSuccess.forEach(function (callback) {
-                            callback(state);
-                        }, this);
-                    }; 
-                } catch (error) {
-                    console.log(error);
-                }
-               
-            }
 
             if (onSuccessCallback) {
                 onSuccess.push(onSuccessCallback);
@@ -46,6 +51,25 @@ Wss = (function () {
             if (onErrorCallback) {
                 onError.push(onErrorCallback);
             }
+        },
+
+        enable: function()
+        {
+            if(websock)
+            {
+                enabled = true;
+
+                return true;
+            }
+
+            return false;
+        },
+
+        disable: function()
+        {
+            enabled = false;
+
+            return true;
         }
     }
 })();
